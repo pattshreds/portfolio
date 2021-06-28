2fa3ec89bd18c8390544a2aa118ccbb9dcc42c10
Jasmine Smith <Jas@Jasmines-MacBook-Air-2.local>
2021-06-28 17:25:15 -0600

    added styling to containers, added nav functionality to the header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import '../styles/headerStyles.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const noUnderline = {
+        textDecoration: 'none',
+    };
     return (
         <>
             <div id='headerTxt'>
-                <h2 id='header'>Patrick McGuigan</h2>
+                <Link style={noUnderline} to='/'>
+                    <h2 id='header'>Patrick McGuigan</h2>
+                </Link>
                 <h5 id='subhead'>Full Stack Software Engineer</h5>
             </div>
         </>
